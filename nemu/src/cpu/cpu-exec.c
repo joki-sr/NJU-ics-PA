ebaaa3a7a62676807849ce96523e2adab6e3159d
tracer-ics2023 <tracer@njuics.org>
2024-08-27 19:10:24 -0700

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:10:24 up 17:38,  1 user,  load average: 1.05, 0.70, 0.69

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,6 +40,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
 
+// 让CPU执行当前PC指向的一条指令, 然后更新PC.
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
