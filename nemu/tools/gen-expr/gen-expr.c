c35530fca7983d70d2250288d1c5c8e5e92863ae
tracer-ics2023 <tracer@njuics.org>
2024-09-04 16:17:52 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:17:52 up 11:34,  1 user,  load average: 0.34, 0.46, 0.46

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -20,43 +20,83 @@
 #include <assert.h>
 #include <string.h>
 
+#ifndef BUF_LEN
+#define BUF_LEN 65536
+#endif
+
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static char buf[BUF_LEN] = {};
+static char code_buf[BUF_LEN + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
+"  unsigned result = %s; "  //%s将用来插入 buf 中的表达式
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
 
+void gen_num(void){
+  //static int seed = time(0);
+  uint32_t num = srandom( (unsigned int)time(NULL));
+  snprintf(buf, sizeof(buf),"%u",num);
+}
+
+void gen_rand_op(void){
+  switch(choose(4)){
+    case 0:gen('+');break;
+    case 1:gen('-');break;
+    case 2:gen('*');break;
+    default:gen('/');break;
+  }
+}
+
+void gen(char ch){
+  if( sizeof(buf) > BUF_LEN)return;
+  strcat(buf, ch);
+}
+
+// todo
 static void gen_rand_expr() {
   buf[0] = '\0';
+  switch (choose(3)) {
+    case 0: 
+      gen_num();   if( sizeof(buf) > BUF_LEN)return;
+      break;
+    case 1: 
+      gen('(');    if( sizeof(buf) > BUF_LEN)return;
+      gen_rand_expr();    if( sizeof(buf) > BUF_LEN)return;
+      gen(')');    if( sizeof(buf) > BUF_LEN)return;
+      break;
+    default: 
+      gen_rand_expr();    if( sizeof(buf) > BUF_LEN)return;
+      gen_rand_op();    if( sizeof(buf) > BUF_LEN)return;
+      gen_rand_expr();    if( sizeof(buf) > BUF_LEN)return;
+      break;
+  }
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
-  srand(seed);
+  srand(seed);  //初始化随机数生成器
   int loop = 1;
   if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
+    sscanf(argv[1], "%d", &loop); // 从命令行参数获取循环次数 loop（默认值为 1）
   }
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
 
-    sprintf(code_buf, code_format, buf);
+    sprintf(code_buf, code_format, buf); // code_buf<-code_format<-buf
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
-    fputs(code_buf, fp);
+    fputs(code_buf, fp);    // 将生成的 C 代码写入到 /tmp/.code.c 文件
     fclose(fp);
-
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    // 调用 gcc 编译 /tmp/.code.c 成可执行文件 /tmp/.expr
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr"); 
     if (ret != 0) continue;
 
-    fp = popen("/tmp/.expr", "r");
+    fp = popen("/tmp/.expr", "r"); // popen 运行 /tmp/.expr 并读取其输出
     assert(fp != NULL);
 
     int result;
