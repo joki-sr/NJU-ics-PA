716fe0b5fe3a6d9e5cded0d180739035a421f72e
tracer-ics2023 <tracer@njuics.org>
2025-01-29 21:05:02 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:05:02 up 1 day,  5:24,  1 user,  load average: 0.78, 0.57, 0.40

---
+++ nemu/src/isa/riscv32/inst.c
@@ -0,0 +1,94 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include "local-include/reg.h"
+#include <cpu/cpu.h>
+#include <cpu/ifetch.h>
+#include <cpu/decode.h>
+
+#define R(i) gpr(i)
+#define Mr vaddr_read
+#define Mw vaddr_write
+
+enum {
+  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_N, // none
+  TYPE_JAL,
+};
+
+#define src1R() do { *src1 = R(rs1); } while (0)
+#define src2R() do { *src2 = R(rs2); } while (0)
+#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immJAL() do { *imm = BITS(i, 20, 20) | BITS(i, 10, 1) | BITS(i, 11, 11) | BITS(i, 19, 12); } while (0)
+
+// 据传入的指令类型type来进行操作数的译码, 译码结果将记录到函数参数rd, src1, src2和imm中, 
+// 它们分别代表目的操作数的寄存器号码, 两个源操作数和立即数.
+static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
+  uint32_t i = s->isa.inst.val;
+  int rs1 = BITS(i, 19, 15);
+  int rs2 = BITS(i, 24, 20);
+  *rd     = BITS(i, 11, 7);
+  switch (type) {
+    case TYPE_I: src1R();          immI(); break;
+    case TYPE_U:                   immU(); break;
+    case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_JAL:               immJAL(); break;
+  }
+}
+
+static int decode_exec(Decode *s) {
+  int rd = 0;
+  word_t src1 = 0, src2 = 0, imm = 0;
+  s->dnpc = s->snpc;
+
+#define INSTPAT_INST(s) ((s)->isa.inst.val)
+#define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
+  decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+  __VA_ARGS__ ; \
+}
+//U
+  INSTPAT_START();
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+//I
+  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
+  INSTPAT("???????????? ????? 000 ????? 0010011",   addi   , I, R(rd) = R(src1) + imm);
+  INSTPAT("000000000001 00000 000 00000 1110011",   ebreak , I, /*throw breakpoint exception*/);
+//S
+  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
+  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2)); // store 1word=4bytes
+//J
+  INSTPAT("???????????? ???????? ????? 1101111"   , jal    , JAL, R(rd) = s->pc + 4, s->pc += imm);
+  
+  // Pseudo
+  // li = addi // INSTPAT("?????????? ?????????? ????? 0110111", li_lui, U, R(rd) = imm);
+  // mv = addi // OK.
+  // j = jal // OK.
+
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+   // 前面所有的模式匹配规则都无法成功匹配, 该指令视为非法指令inv
+  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  INSTPAT_END();
+
+  R(0) = 0; // reset $zero to 0
+
+  return 0;
+}
+
+int isa_exec_once(Decode *s) {
+  s->isa.inst.val = inst_fetch(&s->snpc, 4);
+  return decode_exec(s);
+}
