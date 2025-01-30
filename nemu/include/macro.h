60e4d701586c0dcd04048bcc9742b32dd355fbaa
tracer-ics2023 <tracer@njuics.org>
2025-01-30 12:04:16 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:04:16 up 1 day,  7:21,  1 user,  load average: 0.76, 0.55, 0.42

--- nemu/include/macro.h
+++ nemu/include/macro.h
@@ -85,7 +85,9 @@
 
 #define BITMASK(bits) ((1ull << (bits)) - 1)
 #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // 位抽取，similar to x[hi:lo] in verilog
-#define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; }) // 符号扩展
+// 符号扩展 SEXT(x, len) << n
+// 字符串x已经有len位，左移n位
+#define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; }) 
 
 #define ROUNDUP(a, sz)   ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
 #define ROUNDDOWN(a, sz) ((((uintptr_t)a)) & ~((sz) - 1))
