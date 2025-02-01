fc8bee78375309b5157265249abf8a4822978499
tracer-ics2023 <tracer@njuics.org>
2025-02-01 14:24:56 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:24:56 up  3:43,  1 user,  load average: 0.64, 0.33, 0.22

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,7 +33,7 @@ enum {
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 // #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); printf("immI=%d\n",*imm); } while(0)
-#define immI() do { *imm = BITS(i,31,20);printf("immI=%d\n",*imm); *imm = SEXT(*imm, 12); /*printf("immI=%d\n",*imm);*/ } while(0)
+#define immI() do { *imm = SEXT(BITS(i,31,20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { \
