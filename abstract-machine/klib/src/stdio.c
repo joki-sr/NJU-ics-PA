8f58ce4598aaeb74a037eed53dffd8584b2f29d8
tracer-ics2023 <tracer@njuics.org>
2025-02-08 11:47:21 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:47:21 up 21:38,  1 user,  load average: 0.61, 0.60, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,28 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void itoa(int num, char *str, int base){
+  int is_neg = (num < 0);
+  int str_idx = 0;
+  char tmp[256];
+  int tmp_idx = 0;
+
+  if(is_neg){
+    str[str_idx++] = '-';
+    num = -num;
+  }
+
+  while(num > 0){
+    tmp[tmp_idx++] = num % base;
+    num /= base;
+  }
+  //reverse
+  while(tmp_idx>0){
+    tmp_idx --;
+    str[str_idx++] = tmp[tmp_idx];
+  }
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +36,26 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list va;
+  va_start(va, fmt);
+  size_t i = 0;
+
+  for(i=0;fmt[i] != '\0';i++){
+    if(fmt[i]=='%'){
+      i++;
+      if(fmt[i]=='s'){
+        // %s
+        strcat(out, va_arg(va,char*));
+      }else if(fmt[i]=='d'){
+        // %d
+        int n = va_arg(va, int);
+        itoa(n, out+strlen(out), 10);
+      }else assert(0);
+    }else{
+      strcat(out, fmt + i);
+    }
+  }
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
