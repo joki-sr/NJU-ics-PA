ea2b14540ca837994236681a7cd370d731126553
tracer-ics2023 <tracer@njuics.org>
2025-02-12 16:24:06 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:24:06 up 12:53,  1 user,  load average: 0.89, 0.65, 0.40

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  printf("aaa\n");
+  // printf("aaa\n");
   for(int i=0;i<32;i++){
     // printf("gpr[%d]\n",i);
     if(cpu.gpr[i] != ref_r->gpr[i]){
@@ -27,7 +27,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       return false;
     }
   }
-  printf("no problem\n");
+  printf("pc:%x no problem\n", cpu.pc);
   return true;
 }
 
