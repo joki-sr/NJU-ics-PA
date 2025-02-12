dbdaddba8792f806f562e0c68d576b5cf4f1b096
tracer-ics2023 <tracer@njuics.org>
2025-02-12 16:17:06 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:17:06 up 12:46,  1 user,  load average: 0.95, 0.41, 0.21

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,9 +18,11 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  printf("aaa");
   for(int i=0;i<32;i++){
     if(cpu.gpr[i] != ref_r->gpr[i]){
       pc = cpu.pc;
+      printf("%d != %d", cpu.gpr[i] , ref_r->gpr[i]);
       return false;
     }
   }
