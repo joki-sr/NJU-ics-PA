7f34979187d415583f39ed1c8a0a7554b0b4917c
tracer-ics2023 <tracer@njuics.org>
2025-02-16 15:25:09 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:25:09 up 1 day,  5:46,  1 user,  load average: 1.17, 0.85, 0.72

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -39,7 +39,7 @@ int printf(const char *fmt, ...) {
   
   va_list args;
   va_start(args, fmt);
-  sprintf(buf, fmt, args);
+  vsprintf(buf, fmt, args);
   va_end(args);  // 结束变参处理
 
   int len =strlen(buf);
@@ -50,7 +50,33 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  size_t i=0,out_idx = 0;
+
+  for(i=0;fmt[i]!='\0';i++){
+    if(fmt[i]=='%'){
+      i++;
+      if(fmt[i]=='s'){
+        char *s = va_arg(ap, char*);
+        size_t len = strlen(s);
+        memcpy(out + out_idx, s, len);
+        out_idx+=len;
+      }else if(fmt[i]=='d'){
+        // %d
+        int n = va_arg(ap, int);
+        char num_str[12];
+        itoa(n, num_str, 10);
+        size_t len = strlen(num_str);
+        memcpy(out + out_idx, num_str, len);
+        out_idx += len;
+      }else assert(0);
+    }else{
+      // strcat(out, fmt + i); don't use this
+      out[out_idx++] = fmt[i];
+    }
+  }
+  out[out_idx] = '\0'; // 确保字符串正确终止
+  return out_idx;
 }
 
 // return out lenth
