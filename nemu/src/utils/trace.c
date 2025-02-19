b28834743f87b7c52d9f55e6d377519784102da1
tracer-ics2023 <tracer@njuics.org>
2025-02-19 15:46:33 +0800

    >  compile NEMU 55220905 张siyi Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:46:33 up 1 day, 15:41,  1 user,  load average: 0.92, 0.96, 0.88

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -79,6 +79,7 @@ void dtrace_add(const char *str, bool write){
     dring[dring_idx][0]='r';
   }
   memcpy(dring[dring_idx]+1, str, DRING_LEN-1);
+  printf("dring[%d]=%s\n", dring_idx, dring[dring_idx]);
 }
 
 int dtrace(void){
